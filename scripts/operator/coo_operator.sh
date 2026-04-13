2f9fb95f771f9141bdbf78d743a874ffbd988828
Pradnya.Repal <pradnya.repal@PradnyaRepals-MacBook-Air.local>
2026-04-13 21:24:34 +0530

    removded commenting block for test

--- scripts/operator/coo_operator.sh
+++ scripts/operator/coo_operator.sh
@@ -5,16 +5,17 @@ rm -rf ~/.ansible
 ansible all -m setup -a 'gather_subset=!all'
 cd ${WORKSPACE}/ocp4-playbooks-extras 
 
+
 #added lokicred-secret  
 
-#oc get ns openshift-logging >/dev/null 2>&1 || oc create ns openshift-logging 
 oc -n openshift-logging create secret generic lokicred-secret --from-literal=endpoint=https://s3.jp-tok.cloud-object-storage.appdomain.cloud --from-literal=region=jp-tok --from-literal=bucketnames=cos-standard-upi-validation --from-literal=access_key_id=${COS_STANDARD_KEY} --from-literal=access_key_secret=${COS_STANDARD_SECRET}
 
-FILE_PATH="./playbooks/roles/ocp-cluster-logging/files/validate-urls.yml"
+#FILE_PATH="./playbooks/roles/ocp-cluster-logging/files/validate-urls.yml"
 
-echo "Commenting block in $FILE_PATH"
+#commenting path in file
+#echo "Commenting block in $FILE_PATH"
 
-sed -i '/# Set default value for Syslog host/,/cw_secret is undefined/s/^/#/' "$FILE_PATH"
+#sed -i '/# Set default value for Syslog host/,/cw_secret is undefined/s/^/#/' "$FILE_PATH"
 
 cp examples/ocp_coo_vars.yaml coo_vars.yaml
 
@@ -27,9 +28,8 @@ sed -i \
 -e "s|ocp_cluster_logging:.*$|ocp_cluster_logging: false|g" \
 -e "s|cluster_log_forwarder:.*$|cluster_log_forwarder: false|g" \
 -e "s|coo_catalogsource_image:.*$|coo_catalogsource_image: \"${COO_CATALOGSOURCE_IMAGE}\"|" \
+-e "s|coo_enable_global_secret :.*$|coo_enable_global_secret: false|g" \
 coo_vars.yaml
-
-sed -i '/Include the global pull-secret update role/,/name: global-secret-update/ s/^/#/' playbooks/roles/ocp-coo/tasks/install.yaml
 
 cat coo_vars.yaml
 
@@ -41,14 +41,7 @@ sed -i "s|ssh|ssh ansible_ssh_private_key_file=${WORKSPACE}/deploy/id_rsa|g" coo
 
 cat coo-inventory
 
-#debug path exist
-cd playbooks/roles/ocp-coo 
-ls 
-cd files
-cat ImageDigestMirrorSet.yml
-ls
-cd ..
-cd tasks
+cd playbooks/roles/ocp-coo/tasks 
 sed -i '/- name: Run ImageDigestMirrorSet/a\          delegate_to: localhost' install.yaml
 sed -i '/- name: Create CatalogSource/a\          delegate_to: localhost' install.yaml
 sed -i '/- name: Run CatalogSource/a\          delegate_to: localhost' install.yaml
@@ -56,8 +49,8 @@ cd ../../../..
 
 # Install dependency
 python3 -m pip install --break-system-packages kubernetes openshift
-
 python3 -c "import kubernetes; print('OK')"
+
 ansible-galaxy collection install kubernetes.core
 ansible-playbook  -i coo-inventory -e @coo_vars.yaml playbooks/ocp-coo.yml -vvv
 
