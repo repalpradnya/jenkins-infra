338e12cfecff00a556b6ae4951596ea5c489864b
Pradnya.Repal <pradnya.repal@PradnyaRepals-MacBook-Air.local>
2026-04-16 11:24:33 +0530

    added secret to bastion

--- scripts/operator/coo_operator.sh
+++ scripts/operator/coo_operator.sh
@@ -5,18 +5,6 @@ rm -rf ~/.ansible
 ansible all -m setup -a 'gather_subset=!all'
 cd ${WORKSPACE}/ocp4-playbooks-extras 
 
-
-#added lokicred-secret  
-
-oc -n openshift-logging create secret generic lokicred-secret --from-literal=endpoint=https://s3.jp-tok.cloud-object-storage.appdomain.cloud --from-literal=region=jp-tok --from-literal=bucketnames=cos-standard-upi-validation --from-literal=access_key_id=${COS_STANDARD_KEY} --from-literal=access_key_secret=${COS_STANDARD_SECRET}
-
-#FILE_PATH="./playbooks/roles/ocp-cluster-logging/files/validate-urls.yml"
-
-#commenting path in file
-#echo "Commenting block in $FILE_PATH"
-
-#sed -i '/# Set default value for Syslog host/,/cw_secret is undefined/s/^/#/' "$FILE_PATH"
-
 cp examples/ocp_coo_vars.yaml coo_vars.yaml
 
 sed -i \
@@ -36,8 +24,19 @@ cp examples/inventory ./coo-inventory
 sed -i "s|localhost|${BASTION_IP}|g" coo-inventory
 sed -i 's/ansible_connection=local/ansible_connection=ssh/g' coo-inventory
 sed -i "s|ssh|ssh ansible_ssh_private_key_file=${WORKSPACE}/deploy/id_rsa|g" coo-inventory
-
 cat coo-inventory
+
+ssh -i ${WORKSPACE}/deploy/id_rsa root@${BASTION_IP} <<EOF
+oc create secret generic lokicred-secret \
+  --from-literal=endpoint=https://s3.jp-tok.cloud-object-storage.appdomain.cloud \
+  --from-literal=region=jp-tok \
+  --from-literal=bucketnames=cos-standard-upi-validation \
+  --from-literal=access_key_id=${COS_STANDARD_KEY} \
+  --from-literal=access_key_secret=${COS_STANDARD_SECRET} \
+  -n openshift-logging \
+  --dry-run=client -o yaml | oc apply -f -
+EOF
+
 
 cd playbooks/roles/ocp-coo/tasks 
 sed -i '/- name: Run ImageDigestMirrorSet/a\          delegate_to: localhost' install.yaml
@@ -52,11 +51,5 @@ python3 -m pip install --break-system-packages kubernetes openshift
 python3 -c "import kubernetes; print('OK')"
 
 ansible-galaxy collection install kubernetes.core
-
-#debug for secret
-oc whoami
-oc config current-context
-oc project
-oc get secret -n openshift-logging | grep lokicred-secret
 ansible-playbook  -i coo-inventory -e @coo_vars.yaml playbooks/ocp-coo.yml -vvv
 
